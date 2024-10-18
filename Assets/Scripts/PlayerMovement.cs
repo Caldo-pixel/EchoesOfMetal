4205842bd8a4276f76404932b5ad46fb6a0a979a
Caldo-pixel <200602048@student.georgianc.on.ca>
2024-10-18 17:01:44 -0400

    added ghost mode sprite switch and jump (very not good)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    public float speed = 4f;
-    public float jumpPower = 8f;
+    private float speed = 4f;
+    private float jumpPower = 8f;
+
+    public Sprite Ghost;
+
+    SpriteRenderer sr;
+
+    void Start()
+        {
+            sr = GetComponent<SpriteRenderer>();
+
+        }
 
     // Update is called once per frame
     void Update()
     {
-        float inputX = Input.GetAxisRaw("Horizontal");
 
-        Vector3 inputVector = new Vector3(inputX, 0, 0) * speed * Time.deltaTime;
-        transform.position += inputVector;
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+
+        //changes sprite when F is pressed
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            this.gameObject.GetComponent<SpriteRenderer>().sprite = Ghost;
+        }
+
+        //if Space is pressed, add jumpPower to movement
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 inputVector = new Vector3(inputX, jumpPower, 0) * speed * Time.deltaTime;
+            transform.position += inputVector;
+        }
+        else
+        {
+            Vector3 inputVector = new Vector3(inputX, 0, 0) * speed * Time.deltaTime;
+            transform.position += inputVector;
+        }
+
+        //while (this.gameObject.GetComponent<SpriteRenderer>().sprite = Ghost)
+        //{
+        //     Vector3 inputVector = new Vector3(inputX, inputY, 0) * speed * Time.deltaTime;
+        //     transform.position += inputVector;
+        //     GetComponent<Rigidbody2D>().gravityScale = 0;
+        //}
+
     }
 }
